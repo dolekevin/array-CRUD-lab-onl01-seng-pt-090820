46ef1fd9f18ac8b09de5184e189652556234a4d1
Kevin Dole <dole.kevin@gmail.com>
2020-09-20 02:07:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,14 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index[1]
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array[0]
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last_element_from_array[-1]
+  array[-1]
 end
