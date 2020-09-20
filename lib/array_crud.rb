783c94e1ad9f919cc95112dc31c2d5e123aa655d
Kevin Dole <dole.kevin@gmail.com>
2020-09-20 02:04:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  create_an_empty_array = []
 end
 
 def create_an_array
-  
+  create_an_array = ["Bob", "Jim", "Bill", "Jeff"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  puts add_element_to_end_of_array.push("lone") 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift("lane")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
+  retrieve_first_element_from_array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array[-1]
 end
